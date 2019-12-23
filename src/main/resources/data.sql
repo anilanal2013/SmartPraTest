c21e6bfd2c2f059616a2deb5eb76c27def9ace55
anilanal2013 <anilanal2013@gmail.com>
2019-12-23 12:10:07 +0530

    Update data.sql
    Insert script change.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
---INSERT INTO USERS (ID, NAME, EMAILID) VALUES (1, 'Anil K Anal','anilanal@gmail.com');
---INSERT INTO USERS (ID, NAME, EMAILID) VALUES (2, 'Subbu Subramanian','subbu@yahoo.co.in');
---INSERT INTO USERS (ID, NAME, EMAILID) VALUES (3, 'Balki Mishra','balki@gmail.com');
+INSERT INTO USERS (ID, NAME, EMAILID) VALUES (4, 'Anil K Anal-4','anilanal@gmail.com');
+INSERT INTO USERS (ID, NAME, EMAILID) VALUES (5, 'Subbu Subramanian-5','subbu@yahoo.co.in');
+INSERT INTO USERS (ID, NAME, EMAILID) VALUES (6, 'Balki Mishra-6','balki@gmail.com');
