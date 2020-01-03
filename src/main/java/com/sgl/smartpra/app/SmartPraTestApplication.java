618778d0e3159aa2fa9a9467ce63c8f1bc502a6f
Anil Kumarr Anal <anilkumar.anal@sutherlandglobal.com>
2020-01-03 19:47:48 +0530

    DB connectivity.

--- src/main/java/com/sgl/smartpra/app/SmartPraTestApplication.java
+++ src/main/java/com/sgl/smartpra/app/SmartPraTestApplication.java
@@ -1,11 +1,7 @@
 package com.sgl.smartpra.app;
 
-import java.sql.SQLException;
-import org.h2.tools.Server;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -15,9 +11,5 @@ public class SmartPraTestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SmartPraTestApplication.class, args);
 	}
-
-	@Bean(initMethod = "start", destroyMethod = "stop")
-	public Server h2WebConsoleServer() throws SQLException {
-		return Server.createWebServer("-web", "-webAllowOthers");
-	}
+	
 }
